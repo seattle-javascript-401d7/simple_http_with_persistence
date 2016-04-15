4af483419eee2cecf2887925136edb31da1fa8c5
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-15 09:16:49 -0700

    set up tests

--- carrie-short/test/server_test.js
+++ carrie-short/test/server_test.js
@@ -11,7 +11,35 @@ describe('the http server', () => {
       done();
     });
   });
-  it('should accept get requests to /notes and respond with list of notes');
-  it('should accept json post requests to /notes and save the body of the json object in a file');
-  it('should 404 on bad requests');
+  it('should accept get requests to /notes and respond with list of notes', (done) => {
+    request('localhost:3000')
+    .get('/notes')
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql('something');
+      done();
+    });
+  });
+  it('should accept json post requests to /notes and save the body in a file', (done) => {
+    request('localhost:3000')
+    .post('/notes')
+    .send({ 'noteBody': 'Hello World' })
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql('File Created');
+      done();
+    });
+  });
+  it('should 404 on bad requests', (done) => {
+    request('localhost:3000')
+    .get('/badroute')
+    .end((err, res) => {
+      expect(err.toString()).to.eql('Error: Not Found');
+      expect(res).to.have.status(404);
+      expect(res.text).to.eql('404 - Page not found');
+      done();
+    });
+  });
 });
