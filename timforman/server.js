c6f3dcf0625379e5f5920eae866984db163a835c
t4man <timothywforman@gmail.com>
2016-04-15 09:17:56 -0700

    edit server.js

--- timforman/server.js
+++ timforman/server.js
@@ -1,39 +1,40 @@
+'use strict';
+
 const fs = require('fs');
 const http = require('http');
-const dataDir = __dirname + '/data/';
 
-var fileCount = 0;
-
-var server = module.exports = exports = http.createServer((req, res) => {
-  if (req.url.startsWith('/greet')) {
-    var filename = req.url.split('/')[2];
-    if (req.method === 'GET') {
-      fs.readFile(dataDir + filename + '.json', (err, data) => {
-        if (err) return err;
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(data.toString());
-        res.end();
+const server = module.exports = exports = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/list') {
+    fs.readdir(__dirname + '/data/', (err, data) => {
+      if (err) return err;
+      var fileList = '<h2>Files in data folder</h2><ol>';
+      data.forEach((data) => {
+        fileList += '<li>' + data + '</li>';
       });
-    }
-    if (req.method === 'POST') {
-      var body = '';
-      filename = filename || ++fileCount;
-      req.pipe(fs.createWriteStream(dataDir + filename + '.json'));
-      req.on('data', (chunk) => {
-        body += chunk;
-      });
-      req.on('end', () => {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(body);
-        res.end();
-      });
-    }
-    res.writeHead(404, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify({ msg: 'Not Found'}));
-    res.end();
+      fileList += '</ol>';
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(fileList + '\n');
+      return res.end();
+    });
+    return;
   }
+  if (req.method === 'POST' && req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    req.on('data', (data) => {
+      var path = 'data/' + Date() + '.json';
+      res.write(data);
+      fs.writeFile(path, data, (err) => {
+        if (err) return 'Error';
+        console.log(Date() + ' file saved.');
+        return res.end();
+      });
+    });
+    return;
+  }
+  res.writeHead(404, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify({ msg: 'Not Found'}));
+  res.end();
 });
-
 server.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
