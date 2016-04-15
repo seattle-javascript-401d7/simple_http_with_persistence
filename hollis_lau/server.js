df8a7c5f175d3283dd6a993603185c859902bd2b
Hollis Lau <hollislau@hotmail.com>
2016-04-15 10:23:37 -0700

    Add get and post routes for /notes; add corresponding tests

--- hollis_lau/server.js
+++ hollis_lau/server.js
@@ -1,12 +1,41 @@
 const http = require("http");
 const fs = require("fs");
-const path = require("path");
 
 var server = module.exports = http.createServer((req, res) => {
+  var fileName;
+  var jsonFile;
+
   if (req.method === "GET" && req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.write("Tower, this is Ghostrider, requesting a flyby.");
     return res.end();
+  }
+
+  if (req.method === "GET" && req.url === "/notes") {
+    fs.readdir(__dirname + "/data", (err, files) => {
+      if (err) return process.stderr.write(err + "\n");
+
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write(files.join("\n"));
+      res.end();
+    });
+    return;
+  }
+
+  if (req.method === "POST" && req.url === "/notes") {
+    fs.readdir(__dirname + "/data", (err, files) => {
+      if (err) return process.stderr.write(err + "\n");
+      fileName = String(files.length + 1) + ".json";
+      jsonFile = fs.createWriteStream(__dirname + "/data/" + fileName);
+      req.pipe(jsonFile);
+    });
+
+    req.on("end", () => {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write(fileName);
+      res.end();
+    });
+    return;
   }
 
   res.writeHead(404, { "Content-Type": "text/plain" });
--- hollis_lau/test/server_test.js
+++ hollis_lau/test/server_test.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
@@ -30,6 +31,36 @@ describe("server", () => {
         expect(err).to.eql(err);
         expect(res).to.have.status(404);
         expect(res.text).to.eql("Negative Ghostrider, the pattern is full.");
+        done();
+      });
+  });
+
+  it("should accept a post request to /notes and save a JSON file", (done) => {
+    var json = '{"Maverick": "Pete Mitchell"}';
+
+    request("localhost:3000")
+      .post("/notes")
+      .send(json)
+      .end((err, res) => {
+        var fileContent = fs.readFileSync(__dirname + "/../data/" + res.text, "utf8");
+
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(fileContent).to.eql(json);
+        done();
+      });
+  });
+
+  it("should respond to /notes with a list of all json files", (done) => {
+    request("localhost:3000")
+      .get("/notes")
+      .end((err, res) => {
+        var fileList = fs.readdirSync(__dirname + "/../data").join("\n");
+
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.text).to.eql(fileList);
+        process.stdout.write(fileList);
         done();
       });
   });
