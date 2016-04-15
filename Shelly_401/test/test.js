9db5a854cfb7003a5b01a16f69242217c11c3974
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-15 11:52:58 -0700

    adding files

--- Shelly_401/test/test.js
+++ Shelly_401/test/test.js
@@ -5,14 +5,14 @@ const expect = chai.expect;
 const request = chai.request;
 require(__dirname + '/../server');
 
-describe('vanilla server test', () => {
-  it('should accept GET requests to /thisroute', (done) => {
+describe('http with storage', () => {
+  it('should accept GET requests to /notes', (done) => {
     request('localhost:3000')
-    .get('/thisroute')
+    .get('/notes')
     .end((err, res) => {
     expect(err).to.eql(null);
     expect(res).to.have.status(200);
-    expect(res.text).to.eql("something I'll put in later");
+    expect(res.text).to.equal('Lorem ipsum');
       done();
   });
   });
