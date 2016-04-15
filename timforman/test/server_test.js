d41eba932a208f16481cc4e2495cb2149aa136ae
t4man <timothywforman@gmail.com>
2016-04-15 09:15:50 -0700

    add server test

---
+++ timforman/test/server_test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const server = require(__dirname + '/../server');
+chai.use(chaiHTTP);
+const expect = chai.expect;
+const request = chai.request;
+const fs = require('fs');
+var jsonString = '{"msg": "test"}';
+
+describe('test http server with persistence', () => {
+  before(() => {
+    fs.readdir('/data/', (err, data) => {
+      if (err) return 'An error has occurred.';
+      console.log(data);
+    });
+  });
+  it('should run GET request', (done) => {
+    request('localhost:3000')
+    .get('/list')
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+  it('should check POST request and return a JSON file', (done) => {
+    request('localhost:3000')
+    .post('/')
+    .send(jsonString)
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql('{"msg": "test"}');
+      done();
+    });
+  });
+  after(() => {
+    server.close();
+  });
+});
