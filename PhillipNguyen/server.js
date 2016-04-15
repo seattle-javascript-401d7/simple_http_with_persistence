1bc4ea5cfcbb9d1684c642d2d2727c874354df9c
Phil Nguyen <philnguyen@Phils-MacBook-Pro.local>
2016-04-15 00:52:15 -0700

    creating get post request at route data

---
+++ PhillipNguyen/server.js
@@ -0,0 +1,25 @@
+'use strict';
+const http = require('http');
+const fs = require('fs');
+
+let counter = fs.readdirSync(__dirname + '/data/').length;
+
+module.exports = exports = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/data') {
+    counter += 1;
+
+    let logFile = fs.createWriteStream(__dirname + '/data/' + counter + '.json');
+    req.pipe(logFile);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write('{"msg": "success"}');
+    return res.end();
+  }
+
+  if (req.method === 'GET' && req.url === '/data') {
+   res.writeHead(200, { 'Content-Type': 'application/json' });
+   fs.readdirSync(__dirname + '/data/').forEach((e) => {
+     res.write(e + '\n');
+   });
+   return res.end();
+  }
+}).listen(3000, () => console.log('Server is up!'));
