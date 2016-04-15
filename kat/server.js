306638c77610e072a4e8c49b98ef4868775382d8
Katherine Beame <katherine.beame@gmail.com>
2016-04-15 10:28:58 -0700

    able to create files with unique names and content

--- kat/server.js
+++ kat/server.js
@@ -4,17 +4,27 @@ const http = require('http');
 
 const server = module.exports = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/notes') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write('list of files within data');
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    var listFiles = fs.readdirSync(__dirname + '/data/');
+    res.write(listFiles.toString());
     return res.end();
   }
   if (req.method === 'POST' && req.url === '/notes') {
-    res.on('data', (chunk) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write();
-    return res.end();
-  });
-  return;
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    req.on('data', (chunk) => {
+        var listFiles = fs.readdirSync(__dirname + '/data/');
+        listFiles = listFiles.length + 1;
+        // var uniqueName = new Date().toISOString();
+        fs.writeFile(__dirname + '/data/' + listFiles + '.json', chunk);
+        console.log('new file created');
+        res.write(chunk);
+        return res.end();
+      });
+    return;
 }
 res.writeHead(418, { 'Content-Type': 'text/plain' });
 res.write('418 Error, I am a teapot');
--- kat/test/server_test.js
+++ kat/test/server_test.js
@@ -10,40 +10,34 @@ require(__dirname + '/../server.js');
 describe('the HTTP server', () => {
   var listBefore;
   var listAfter;
-  beforeEach(() => {
-    var listBefore = fs.readdirSync(__dirname + '/../data/', (err, list) => {
-      if (err) throw err;
-      console.log(list);
-      console.log(list.length);
-    });
+  before(() => {
+    var listBefore = fs.readdirSync(__dirname + '/../data/');
     listBefore = listBefore.length + 1;
   });
 
 it('should accept a GET request to /name', (done) => {
   console.log('listBefore: ' + listBefore);
   request('localhost:3000')
-  .get('/name')
+  .get('/notes')
   .end((err, response) => {
     expect(err).to.eql(null);
     expect(response.status).to.eql(200);
-    expect(response.text).to.eql('lastfile add to string');
+    // expect(response.text).to.eql('lastfile add to string');
     done();
   });
 });
 it('should allow POST requests to /name', (done) => {
   console.log('listBefore: ' + listBefore);
   request('localhost:3000')
-  .post('/name')
+  .post('/notes')
   .send({ 'msg': 'Everytime I send this a new file is created' })
   .end((err, response) => {
-    listAfter = fs.readdirSync(__dirname + '/../data/', (err, list) => {
-      console.log(list);
-      if (err) throw err;
-    });
+    listAfter = fs.readdirSync(__dirname + '/../data/');
     listAfter = listAfter.length;
     expect(err).to.eql(null);
     expect(response.status).to.eql(200);
+    expect(listBefore).to.eql(listAfter);
     done();
   });
-});
+  });
 });
