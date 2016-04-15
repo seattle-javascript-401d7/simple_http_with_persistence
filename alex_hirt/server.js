708a981fb9be6956b1d3c14743ee6d98e3b41447
Philip A. Hirt <phirt13@gmail.com>
2016-04-14 22:37:39 -0700

    added server.js basic

--- alex_hirt/server.js
+++ alex_hirt/server.js
@@ -1,1 +1,12 @@
+const http = require('http');
+const routes = require(__dirname + '/lib/router.js');
 
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((request, response) => {
+  response.write('<h1>Hello</h1>');
+});
+
+server.listen(port, () => {
+  process.stdout.write('Server listening on localhost:' + port + '\n');
+});
