2ba7da280d52fa8795ea4e948d5a2e995a67e09f
Philip A. Hirt <phirt13@gmail.com>
2016-04-14 22:23:25 -0700

    wrote gulpfile.js

--- alex_hirt/gulpfile.js
+++ alex_hirt/gulpfile.js
@@ -1,1 +1,34 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
 
+var allScripts = ['./*.js', './lib/**/*.js', './test/**/*.js'];
+
+gulp.task('lint', () => {
+  return gulp.src(allScripts)
+    .pipe(eslint({
+      envs: [
+        'es6',
+        'mocha'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:watch', () => {
+  gulp.watch(allScripts, ['lint']);
+});
+
+var testScripts = ['./test/**/*.js'];
+
+gulp.task('mocha', () => {
+  return gulp.src(testScripts)
+    .pipe(mocha({ 'reporter': 'nyan' }));
+});
+
+gulp.task('mocha:watch', () => {
+  gulp.watch(testScripts, ['mocha']);
+});
+
+gulp.task('all', ['lint']);
+gulp.task('default', ['all']);
