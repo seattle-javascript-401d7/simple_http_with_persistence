af9206908dda2aeafba8dbd52bbfa2dd1ac5a320
KMSkelton <kmskelton@outlook.com>
2016-04-16 14:48:38 -0700

    cleanup

--- skelton/lib/server.js
+++ skelton/lib/server.js
@@ -5,9 +5,7 @@ const Router = require(__dirname + '/router.js');
 var dataPath = __dirname + '/../data/';
 var router = new Router()
 .get('/awesomeUrl', (req, res) => {
-  console.log(dataPath);
   fs.readdir(dataPath, (err, files) => {
-    console.log(files);
     if (err) return process.stdout.write(err);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write(JSON.stringify(files));
